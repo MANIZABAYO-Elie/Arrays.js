458b49539d13212def5ac5797fed31ce74c7b31f
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-06-24 22:29:51 +0200

    arrays updated

--- arrays.js
+++ arrays.js
@@ -1,7 +1,10 @@
 let fruits= ["Mangoes","Bananas","oranges"]
 let n = fruits.length
-newArray1 = fruits.push("apples")
-console.log(newArray1);
+newArray = fruits.push("apples")
+console.log(newArray);
 console.log(fruits);
 let newArray2 = fruits.pop()
 console.log(newArray2);
+let  array3 = fruits.unshift("pineapple")
+console.log(array3);
+
